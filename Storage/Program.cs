c6eaf7d20b7a506174affd814f10946f98952153
User <User@DESKTOP-62I7PTD>
2024-10-19 16:13:55 +0700

    Добавьте файлы проекта.

---
+++ Storage/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                DataClasses1DataContext db =
+               new DataClasses1DataContext();
+                var queryResults = from c in db.Product
+                                   where c.costPrice > 200
+                                   select new
+                                   {
+                                       ID = c.Id,
+                                       Name = c.Name,
+                                       Price = c.costPrice
+                                   };
+                foreach (var item in queryResults)
+                {
+                    Console.WriteLine(item);
+                }
+        }
+    }
+}
